949fbebe9d294a9373f8237c02fef1ceab928661
sharadical <sharadd19@gmail.com>
2025-04-10 19:32:17 +0100

    gemini flash works

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,12 @@
+from pathlib import Path
+from dotenv import load_dotenv
+env_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=env_path)
 from fastapi import FastAPI
+from routes import chat, stream, agent
+
 
 app = FastAPI()
-
+app.include_router(chat.router, prefix="/chat")
+app.include_router(stream.router, prefix="/stream")
+app.include_router(agent.router, prefix="/agent")
